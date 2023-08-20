ab2f91183671a7a8f0bc282f4673cca34b7279e5
Nik Uzair <nikuzairsc@gmail.com>
2023-08-20 14:16:53 +0800

    Handling the special ClassName Case

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import { GoBell } from 'react-icons/go';
 import Button from './Button';
 
 function App() {
+  const handleClick = () => {
+    console.log('Click!');
+  };
   return (
     <div>
       <div>
-        <Button success rounded outline>
+        <Button success rounded outline className="mb-5" onClick={handleClick}>
           <GoBell />
           Click me!
         </Button>
       </div>
       <div>
-        <Button danger outline>
+        <Button danger outline onClick={handleClick}>
           Buy now!
         </Button>
       </div>
