30ecfa3f5d443d2358bf9c1bccde2146e00a64de
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 07:23:51 +0800

    Implement click label function

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 function Dropdown({ options }) {
-  const renderedItems = (option) => {
-    return <div>{option.label}</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    // setIsOpen(!isOpen); not working if the user spam click
+    setIsOpen((currentIsOpen) => !currentIsOpen);
   };
+
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
