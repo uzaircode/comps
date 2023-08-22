eec5187d976c420b52464a4cffa7c1589f245298
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 11:32:24 +0800

    Handling Control and Command Keys

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,13 +3,21 @@ import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
+
+  // Enable tab command key for both mac & window
   const handleClick = (event) => {
-    event.preventDefault();
+    if (event.crtlKey || event.metaKey) {
+      return;
+    }
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
