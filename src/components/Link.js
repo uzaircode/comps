ca41fd7fc12a606b67dc3daf34b255f10f07695f
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 11:33:37 +0800

    Link Styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,11 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
+  const classes = classNames('text-blue-500');
   // Enable tab command key for both mac & window
   const handleClick = (event) => {
     if (event.crtlKey || event.metaKey) {
@@ -14,7 +16,7 @@ function Link({ to, children }) {
   };
 
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
