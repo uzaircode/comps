bbbf988917bf3bb04ec775633e98eddbe62fb976
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 09:36:49 +0800

    Add useEffect Cleanup

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { GoChevronDown } from 'react-icons/go';
 import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handler = (event) => {
+      console.log(event.target);
+    };
+
+    document.addEventListener('click', handler, true);
+
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
   const handleClick = () => {
     // setIsOpen(!isOpen); not working if the user spam click
