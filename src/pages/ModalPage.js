466123122376c5527cb706c65b1e1740d9159b00
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 12:17:21 +0800

    Toggling Visibility

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -2,7 +2,19 @@ import { useState } from 'react';
 import Modal from '../components/Modal';
 import Button from '../components/Button';
 
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
   };
+  return (
+    <div>
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && <Modal />}
+    </div>
   );
 }
 
