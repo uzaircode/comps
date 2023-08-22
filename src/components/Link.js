32757f2517c578c4a071241bf499195d55c44dd3
Nik Uzair <nikuzairsc@gmail.com>
2023-08-22 11:36:18 +0800

    Custom Navigation Hook

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import useNavigation from '../hooks/use-navigation';
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
 
   const classes = classNames('text-blue-500');
+
   // Enable tab command key for both mac & window
   const handleClick = (event) => {
     if (event.crtlKey || event.metaKey) {
