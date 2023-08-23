a519c0eba4e64351383c7322f29dc77c59cc2658
Nik Uzair <nikuzairsc@gmail.com>
2023-08-23 11:02:45 +0800

    Creating a Reusable Table

---
+++ src/components/Table.js
@@ -0,0 +1,5 @@
+function Table() {
+  return <p>Hi!</p>;
+}
+
+export default Table;
---
+++ src/pages/TablePage.js
@@ -0,0 +1,6 @@
+import Table from '../components/Table';
+function Tablepage() {
+  return <Table />;
+}
+
+export default Tablepage;
