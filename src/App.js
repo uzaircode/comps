19fdd90051e4e36295af489d2f21677410123b30
Nik Uzair <nikuzairsc@gmail.com>
2023-08-25 20:43:01 +0800

    Creating the Demo Counter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import TablePage from './pages/TablePage';
 import ModalPage from './pages/ModalPage';
+import CounterPage from './pages/CounterPage';
 
 function App() {
   return (
@@ -25,6 +26,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
         </Route>
       </div>
     </div>
