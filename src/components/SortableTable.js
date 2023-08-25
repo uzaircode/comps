930ce9b391539e6fc2a8deea0d933bd55c89a2ff
Nik Uzair <nikuzairsc@gmail.com>
2023-08-25 11:42:00 +0800

    Watching for Header Cell Clicks

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -3,13 +3,22 @@ import Table from './Table';
 function SortableTable(props) {
   const { config } = props;
 
+
+  const handleClick = (label) => {
+    console.log(label);
+  };
+
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
     }
     return {
       ...column,
-      header: () => <th>{column.label} Is sortable </th>,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {column.label} Is sortable{' '}
+        </th>
+      ),
     };
   });
 
